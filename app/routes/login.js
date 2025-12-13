ed03e8f53f4cfd2f72da2e089c1d8ed21f00a084
abdo-bahi <abdo.bahi.bahi@etu.univ-blida.dz>
2025-12-13 21:14:20 +0100

    login with passport.auth() not working

--- app/routes/login.js
+++ app/routes/login.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const ExpressError = require("../utils/ExpressError");
 const User = require("../models/User");
+const passport = require("passport");
 
 router.get("/register", async (req, res) => {
   res.render("user/register");
@@ -11,12 +12,27 @@ router.post("/register", async (req, res) => {
     const { email, username, password } = req.body;
     const user = new User({ email, username });
     const registeredUser = await User.register(user, password); //hash add salt and store
-    req.flash("succes", "welcome to yelp camp!");
+    req.flash("success", "New user registered successfuly");
     res.redirect("/campground");
   } catch (e) {
     req.flash("error", e.message);
     res.redirect("/register");
   }
 });
+router.get("/login", (req, res) => {
+  res.render("user/login");
+});
+//options are local, google ...
+router.post(
+  "/login",
+  // passport.authenticate("local", {
+  //   failureRedirect: "/login",
+  //   failureFlash: true
+  // }),
+  (req, res) => {
+    req.flash("success", "login success!");
+    res.redirect("/campground");
+  }
+);
 
 module.exports = router;
