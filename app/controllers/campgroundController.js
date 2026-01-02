8c152503b11ad828463ba35f3abb23e2d5834251
abdo-bahi <abdo.bahi.bahi@etu.univ-blida.dz>
2026-01-02 21:13:50 +0100

    c1s53 adding routers logic under controllers for campgrounds

---
+++ app/controllers/campgroundController.js
@@ -0,0 +1,93 @@
+const CampGround = require("../models/CampGround");
+
+module.exports.index = async (req, res) => {
+  const camps = await CampGround.find({});
+  //sending a web cookie to be stored in the client's browser cookies
+  res.cookie("name", "Abderrahmane", { signed: true });
+  //const {name = 'visitor'} = req.cookies;
+  //signined true wile passing a secret key while using cookieParser gives us the ability to check if it was the valide (not 'false' value) sent before cookie
+  const { name = "visitor" } = req.signedCookies;
+
+  if (req.session.count) {
+    req.session.count += 1;
+  } else {
+    req.session.count = 1;
+  }
+  const sessionCount = req.session.count;
+  res.render("campgrounds/index", {
+    camps,
+    name,
+    sessionCount,
+  });
+};
+module.exports.saveNew = async (req, res, next) => {
+  if (!req.body) {
+    throw new ExpressError("Invalide Attributes", 422);
+  }
+  const { title, location, image, price, description } = req.body;
+  if (!(title && location && image && price && description)) {
+    throw new ExpressError("Invalide Attributes", 422);
+  }
+  const camp = new CampGround({
+    title: title,
+    location: location,
+    image: image,
+    price: price,
+    description: description,
+  });
+  camp.author = req.user._id;
+  await camp.save();
+  //   creating the flash entity
+  req.flash("success", "Successfully made a camp Ground !");
+  res.redirect(`/campground/${camp._id}`);
+};
+module.exports.addForm = async (req, res) => {
+  if (!req.isAuthenticated()) {
+    req.flash("error", "you must be signed in !");
+    return res.redirect("/login");
+  }
+  res.render("campgrounds/new");
+};
+module.exports.getById = async (req, res) => {
+  //heres is the 'nested' populate where we must populate reviewsIds to reveiws and thier author for each review
+  const camp = await CampGround.findById(req.params.id)
+    .populate({
+      path: "reviews",
+      populate: { path: "author" },
+    })
+    .populate("author");
+
+  console.log("camp : ", camp);
+  if (!camp) {
+    req.flash("error", "cannot find that campground!");
+    return res.redirect("/campground");
+  }
+  res.render("campgrounds/show", { camp });
+};
+
+module.exports.editForm = async (req, res) => {
+  const camp = await CampGround.findById(req.params.id);
+  if (!camp) {
+    req.flash("error", "cannot find that campground!");
+    return res.redirect("/campground");
+  }
+  if (!camp.author.equals(req.user._id)) {
+    req.flash("error", "You do not have permission to do that !");
+    return res.redirect(`/campground/${camp.id}`);
+  }
+  res.render("campgrounds/edit", { camp });
+};
+module.exports.edit = async (req, res) => {
+  // const { title, location, image, description } = req.body;
+  const camp = await CampGround.findById(req.params.id);
+  camp.set(req.body);
+  await camp.save();
+  req.flash("success", "Successfully updated camp Ground !");
+  res.redirect(`/campground/${camp.id}`);
+};
+
+module.exports.delete = async (req, res) => {
+  await CampGround.findByIdAndDelete(req.params.id);
+  req.flash("success", "Successfully deleted CampGround !");
+  res.redirect(`/campground`);
+};
--- app/routes/campCrounds.js
+++ app/routes/campCrounds.js
@@ -3,6 +3,7 @@ const CampGround = require("../models/CampGround");
 const router = express.Router();
 const ExpressError = require("../utils/ExpressError");
 const {isLoggedIn, isCampAuthor} = require("../middleware")
+const campGroundController = require("../controllers/campgroundController");
 // adding the cookies parser to work with cookier
 
 //we can add a router middelware to these routes as in :
@@ -12,97 +13,17 @@ const {isLoggedIn, isCampAuthor} = require("../middleware")
 //     }
 //     res.send('sorry , not an admin');
 // });
-router.get("/", isLoggedIn, async (req, res) => {
-  const camps = await CampGround.find({});
-  //sending a web cookie to be stored in the client's browser cookies
-  res.cookie("name", "Abderrahmane", { signed: true });
-  //const {name = 'visitor'} = req.cookies;
-  //signined true wile passing a secret key while using cookieParser gives us the ability to check if it was the valide (not 'false' value) sent before cookie
-  const { name = "visitor" } = req.signedCookies;
-
-  if (req.session.count) {
-    req.session.count += 1;
-  } else {
-    req.session.count = 1;
-  }
-  const sessionCount = req.session.count;
-  res.render("campgrounds/index", {
-    camps,
-    name,
-    sessionCount,
-  });
-});
+router.get("/", isLoggedIn, campGroundController.index);
 // flash is a way to show an information one time like after creating an object indicating that it was succesfully created
 
-router.post("/", isLoggedIn, async (req, res, next) => {
-  if (!req.body) {
-    throw new ExpressError("Invalide Attributes", 422);
-  }
-  const { title, location, image, price, description } = req.body;
-  if (!(title && location && image && price && description)) {
-    throw new ExpressError("Invalide Attributes", 422);
-  }
-  const camp = new CampGround({
-    title: title,
-    location: location,
-    image: image,
-    price: price,
-    description: description,
-  });
-  camp.author = req.user._id;
-  await camp.save();
-  //   creating the flash entity
-  req.flash("success", "Successfully made a camp Ground !");
-  res.redirect(`/campground/${camp._id}`);
-});
+router.post("/", isLoggedIn, campGroundController.saveNew);
 
-router.get("/new", isLoggedIn, async (req, res) => {
-  if(!req.isAuthenticated()){
-    req.flash('error', 'you must be signed in !');
-    return res.redirect('/login');
-  }
-  res.render("campgrounds/new");
-});
-router.get("/:id", isLoggedIn, async (req, res) => {
-  //heres is the 'nested' populate where we must populate reviewsIds to reveiws and thier author for each review 
-  const camp = await CampGround.findById(req.params.id).populate({
-    path:"reviews",
-    populate: {path : "author"}
-  }).populate("author");
+router.get("/new", isLoggedIn, campGroundController.addForm);
+router.get("/:id", isLoggedIn, campGroundController.getById);
 
-  console.log('camp : ', camp );
-  if (!camp) {
-    req.flash("error", "cannot find that campground!");
-    return res.redirect("/campground");
-  }
-  res.render("campgrounds/show", { camp });
-});
-
-router.get("/:id/edit", isLoggedIn, isCampAuthor, async (req, res) => {
-  const camp = await CampGround.findById(req.params.id);
-  if (!camp) {
-    req.flash("error", "cannot find that campground!");
-    return res.redirect("/campground");
-  }
-  if(! camp.author.equals(req.user._id)){
-    req.flash('error', 'You do not have permission to do that !');
-    return res.redirect(`/campground/${camp.id}`);
-  }
-  res.render("campgrounds/edit", { camp });
-});
-router.patch("/:id", isLoggedIn, isCampAuthor, async (req, res) => {
-  // const { title, location, image, description } = req.body;
-  const camp = await CampGround.findById(req.params.id);
-  camp.set(req.body);
-  await camp.save();
-  req.flash("success", "Successfully updated camp Ground !");
-  res.redirect(`/campground/${camp.id}`);
-});
-router.delete("/:id", isLoggedIn, isCampAuthor,  async (req, res) => {
-  await CampGround.findByIdAndDelete(req.params.id);
-  req.flash("success", "Successfully deleted CampGround !");
-  res.redirect(`/campground`);
-});
+router.get("/:id/edit", isLoggedIn, isCampAuthor, campGroundController.editForm);
+router.patch("/:id", isLoggedIn, isCampAuthor, campGroundController.edit);
+router.delete("/:id", isLoggedIn, isCampAuthor, campGroundController.delete);
 //*******important
 // signing cookies isnt about making them a secret it's about
 //ensuring they are the same cookies sent to us in the first place
